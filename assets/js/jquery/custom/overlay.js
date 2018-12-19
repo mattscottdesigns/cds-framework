8846a18c6e7fce44cbe8d38a7acc107ad421c2ab
Matt Scott <mscott@cdsreg.com>
2018-12-19 12:31:33 -0500

    Update SCSS structure / Finish release notes component

--- assets/js/jquery/custom/overlay.js
+++ assets/js/jquery/custom/overlay.js
@@ -1,8 +1,10 @@
-$.fn.overlay = function () {
-    var element = this;
-
-    this.overlay = $("<div class='modal-backdrop fade show'></div>").hide();
-    element.append(this.overlay);
-
-    return this.overlay
-}
+(function(){
+    $.fn.overlay = function () {
+        var element = this;
+    
+        this.overlay = $("<div class='modal-backdrop fade show'></div>").hide();
+        element.append(this.overlay);
+    
+        return this.overlay
+    }
+})();
