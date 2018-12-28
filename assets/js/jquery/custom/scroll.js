bb26d0b09b8ed9ac4998e1efab40260a0ef5c304
Matt Scott <mscott@cdsreg.com>
2018-12-28 12:03:07 -0500

    Framework updates

--- assets/js/jquery/custom/scroll.js
+++ assets/js/jquery/custom/scroll.js
@@ -1,7 +1,7 @@
 (function(){
     var check = 0;
-    window.addEventListener("scroll", function(){
-        window.scrollY > check ? console.log("down") : console.log("up");
+    $(window).on("scroll", function(){
+        window.scrollY > check ? console.log("down", window.scrollY) : console.log("up", window.scrollY);
         check = window.scrollY
     });
 })();
--- assets/js/jquery/custom/slideout.js
+++ assets/js/jquery/custom/slideout.js
@@ -1,95 +1,101 @@
-$.fn.slideout = function (options) {
-    'use strict';
+;(function(){
+    $.fn.slideout = function (options) {
+        'use strict';
 
-    var slideout = this;
-    var defaultState = {
-        "open": false,
-    };
-    var defaultOptions = {
-        "content": $(".slideout-content"),
-        "direction": "left",
-        "push": false,
-        "width": 260,
-    };
+        var defaultState = {
+            "open": false,
+        };
+        
+        var defaultOptions = {
+            "content": $(".slideout-content"),
+            "direction": "left",
+            "push": false,
+            "width": 260,
+        };
+    
+        var Slideout = function (element, options) {
+            options = options || {};
+            this.defaults = defaultOptions;
+            this.state = defaultState;
+            this.settings = $.extend(defaultOptions, options);
+            this.slideout = element.addClass("slideout-slide");
+            this.content = this.settings.content.addClass("slideout-content");
+    
+            return this.init();
+        };
+    
+        $.extend(Slideout.prototype, {
+    
+            init: function() {
 
-    var Slideout = function (options) {
-        options = options || {};
+                this.close = this.close.bind(this);
+                this.hide = this.hide.bind(this);
+                this.toggle = this.toggle.bind(this);
 
-        this.defaults = defaultOptions;
-        this.state = defaultState;
-        this.settings = $.extend(defaultOptions, options);
-        this.slideout = slideout.addClass("slideout-slide");
-        this.content = this.settings.content.addClass("slideout-content");
+                $(window).on("resize", this.close);
+                $(window).on("click", this.hide);
+                
+                if(this.settings.toggle){
+                    $(this.settings.toggle).on("click", this.toggle);
+                }
 
-        this.init();
-    };
+                return this.render();
+            },
+    
+            hide: function(e){
+                var target = $(e.target);
+                if (target.closest(this.slideout).length == 0 && target.closest(this.settings.toggle).length == 0) {
+                    this.close();
+                }
+    
+                return this
+            },
+    
+            open: function() {
+                this.state.open = true;
+                return this.render();
+            },
+    
+            close: function() {
+                this.state.open = false;
+                return this.render();
+            },
+    
+            toggle: function() {
+                this.state.open = !this.state.open;
+                return this.render();
+            },
+    
+            render: function() {
+                var state = this.state;
+                var settings = this.settings;
+                var open = state.open;
+                var push = settings.push;
+                var direction = settings.direction;
+                var width = settings.width;
+                var styles = {
+                    "slideout": {},
+                    "content": {},
+                }
 
-    Slideout.prototype = {
+                open
+                    ? styles.slideout[direction] = 0
+                    : styles.slideout[direction] = -width;
+    
+                push && open 
+                    ? styles.content[direction] = width 
+                    : styles.content[direction] = 0;
+    
+                styles.slideout.width = width;
 
-        init: function () {
-            if(this.settings.toggle){
-                $(this.settings.toggle).on("click", this.toggle.bind(this));
-            }
-
-            this.slideout.css({ "width": this.settings.width });
-
-            $(window).on("resize", this.close.bind(this));
-            $(window).on("click", this.hide.bind(this));
-
-            return this.render();
-        },
-
-        hide: function(e){
-            var target = $(e.target);
-            if (target.closest(this.slideout).length == 0 && target.closest(this.settings.toggle).length == 0) {
-                this.close();
-            }
-
-            return this
-        },
-
-        open: function () {
-            this.state.open = true;
-            this.render();
-        },
-
-        close: function () {
-            this.state.open = false;
-            this.render();
-        },
-
-        toggle: function () {
-            this.state.open = !this.state.open;
-            this.render();
-        },
-
-        render: function () {
-            var state = this.state;
-            var settings = this.settings;
-            var open = state.open;
-            var push = settings.push;
-            var direction = settings.direction;
-            var width = settings.width;
-            var styles = {
-                "slideout": {},
-                "content": {},
-            }
-
-            open
-                ? styles.slideout[direction] = 0
-                : styles.slideout[direction] = -width;
-
-            push && open 
-                ? styles.content[direction] = width 
-                : styles.content[direction] = 0;
-
-            this.slideout.css(styles.slideout);
-            this.content.css(styles.content);
-
-            return this;
-        },
-
+                this.slideout.css(styles.slideout);
+                this.content.css(styles.content);
+    
+                return this;
+            },
+    
+        });
+        
+        return new Slideout(this, options);
     }
-
-    return new Slideout(options);
-}
+})()
